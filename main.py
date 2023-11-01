199aed0ad27415846ddce50780511186db69d7f0
edwinpro <edwtorre@gmail.com>
2023-11-01 16:13:37 -0500

    Primer commit

--- main.py
+++ main.py
@@ -23,7 +23,20 @@ def home():
     '''
     return fa.presentacion()
 
-    # genre_result = fa.PlayTimeGenre('rpg')
-    # # Convertir el resultado a formato JSON
-    # json_result = json.dumps(genre_result, ensure_ascii=False)
-    # print(json_result)
+@app.get(path = '/playtimegenre',
+          description = """ <font color="blue">
+                        INSTRUCCIONES<br>
+                        1. Haga clik en "Try it out".<br>
+                        2. Ingrese el genero a consultar.<br>
+                        3. Scrollear a "Resposes" para ver la cantidad de dinero gastado por el usuario, el porcentaje de recomendación que realiza el usuario y cantidad de items que tiene el mismo.
+                        </font>
+                        """,
+         tags=["Consultas Generales"])
+def PlayTimeGenre(genero: str = Query(..., 
+                                description="Genero de videojuego", 
+                                examples="EchoXSilence")):
+        
+    genre_result = fa.PlayTimeGenre(genero)
+    # Convertir el resultado a formato JSON
+    json_result = json.dumps(genre_result, ensure_ascii=False)
+    return json_result
